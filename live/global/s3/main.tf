24acc106dbca870fdfd4f01d81ab88ce2ef26a1d
raiden <raidenworley@gmail.com>
2024-10-23 16:22:57 -0300

    add backend

---
+++ live/global/s3/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_version = "1.9.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.72.1"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-assessment-aggrand"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-assessment-locks"
+    key            = "global/s3/terraform.tfstate"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-assessment-aggrand"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-assessment-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
